c063f9df7bfb209fb52d43b9c74335ba86aeb3c0
Dea Arlia <dea.arlia@gmail.com>
2026-05-29 06:39:59 +0700

    m

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const prisma = new PrismaClient();
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json( { limit: '10mb' } ));
+app.use(express.urlencoded({ extended: true, limit: '10mb' })); 
 
 const SECRET_KEY = process.env.SECRET_KEY || "rahasia_tugas_spk_123";
 
